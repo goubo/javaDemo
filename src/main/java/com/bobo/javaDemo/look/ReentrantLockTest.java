816ce904c6b1f3d6b719299381df8af0f05eb771
bobo <goujinbo@outlook.com>
2021-02-19 16:05:48 +0800

    非公平锁

--- src/main/java/com/bobo/javaDemo/look/ReentrantLockTest.java
+++ src/main/java/com/bobo/javaDemo/look/ReentrantLockTest.java
@@ -12,49 +12,48 @@ import java.util.concurrent.locks.ReentrantLock;
  * @since 2021/2/19
  **/
 public class ReentrantLockTest {
-  public static void main(String[] args) {
-    int size = 10;
-    //是否公平锁
-    boolean fair = true;
-
-    CyclicBarrier cyclicBarrier = new CyclicBarrier(size);
-    MyThread myThread = new MyThread(cyclicBarrier, fair);
-    for (int i = 0; i < size; i++) {
-      new Thread(myThread, "t" + i).start();
+    public static void main(String[] args) {
+        int size = 10;
+        //非公平锁
+        boolean fair = false;
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(size);
+        MyThread myThread = new MyThread(cyclicBarrier, fair);
+        for (int i = 0; i < size; i++) {
+            new Thread(myThread, "t" + i).start();
+        }
     }
-  }
 }
 
 class MyThread implements Runnable {
-  private ReentrantLock lock;
-  CyclicBarrier cyclicBarrier;
+    ReentrantLock lock;
+    CyclicBarrier cyclicBarrier;
 
-  public MyThread(CyclicBarrier cyclicBarrier, boolean fair) {
-    this.cyclicBarrier = cyclicBarrier;
-    lock = new ReentrantLock(fair);
+    public MyThread(CyclicBarrier cyclicBarrier, boolean fair) {
+        this.cyclicBarrier = cyclicBarrier;
+        lock = new ReentrantLock(fair);
 
-  }
-
-  @Override
-  public void run() {
-    try {
-      Thread.sleep(1000);
-      cyclicBarrier.await();
-    } catch (InterruptedException | BrokenBarrierException e) {
-      e.printStackTrace();
     }
-    System.out.println(Thread.currentThread().getName() + ":准备抢锁锁");
-    lock.lock();
-    try {
-      System.out.println(Thread.currentThread().getName() + ":抢到了锁");
-      Thread.sleep(1000);
-      System.out.println(Thread.currentThread().getName() + ":准备解锁");
-      Thread.sleep(1000);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    } finally {
-      lock.unlock();
-      System.out.println(Thread.currentThread().getName() + ":已经解锁");
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(1000);
+            cyclicBarrier.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + ":准备抢锁锁");
+        lock.lock();
+        try {
+            System.out.print(Thread.currentThread().getName() + ":抢到了锁");
+            Thread.sleep(1000);
+            System.out.print(Thread.currentThread().getName() + ":准备解锁");
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+            System.out.println(Thread.currentThread().getName() + ":已经解锁");
+        }
     }
-  }
 }
