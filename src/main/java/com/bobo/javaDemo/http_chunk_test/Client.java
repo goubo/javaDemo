17a63966181a9791de84c3bfa10d4d6a7ec7609b
bobo <goujinbo@outlook.com>
2021-02-23 14:30:59 +0800

    添加src

--- src/main/java/com/bobo/javaDemo/http_chunk_test/Client.java
+++ src/main/java/com/bobo/javaDemo/http_chunk_test/Client.java
@@ -16,21 +16,21 @@ import java.io.InputStreamReader;
  * @since 2021/2/6
  **/
 public class Client {
-  public static void main(String[] args) {
-    HttpResponse httpResponse = HttpUtil.createGet("http://127.0.0.1:10789/test/chunk").executeAsync();
-    if (!httpResponse.isOk()) {
-      System.out.println(httpResponse.body());
-      return;
-    }
-    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpResponse.bodyStream()));
-    String str;
-    try {
-      while (StrUtil.isNotEmpty(str = bufferedReader.readLine())) {
-        System.out.println(str);
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    public static void main(String[] args) {
+        HttpResponse httpResponse = HttpUtil.createGet("http://127.0.0.1:10789/test/chunk").executeAsync();
+        if (!httpResponse.isOk()) {
+            System.out.println(httpResponse.body());
+            return;
+        }
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpResponse.bodyStream()));
+        String str;
+        try {
+            while (StrUtil.isNotEmpty(str = bufferedReader.readLine())) {
+                System.out.println(str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-  }
+    }
 }
--- src/main/java/com/bobo/javaDemo/http_chunk_test/Server.java
+++ src/main/java/com/bobo/javaDemo/http_chunk_test/Server.java
@@ -16,37 +16,37 @@ import java.util.concurrent.atomic.AtomicReference;
  * @since 2021/2/6
  **/
 public class Server {
-  public static void main(String[] args) {
-    Vertx vertx = Vertx.vertx();
-    Router router = Router.router(vertx);
-    vertx.createHttpServer().requestHandler(router).listen(10789);
-    router.get("/test/chunk").handler(routingContext -> {
-      System.out.println("收到请求");
-      HttpServerResponse response = routingContext.response();
-      response.setChunked(true);
-      AtomicInteger i = new AtomicInteger();
-      AtomicReference<Long> timerId = new AtomicReference<>();
-      vertx.setPeriodic(1000, id -> {
-        timerId.set(id);
-        String s = LocalDateTime.now().toString();
-        System.out.println("输出中:" + s);
-        response.write(s);
-        response.write("\n");
-        if (i.getAndIncrement() == 10) {
-          vertx.cancelTimer(timerId.get());
-          System.out.println("输出完毕");
-          response.end();//服务端发送结束符
-        }
-      });
-      response.closeHandler(close -> System.out.println("response close"));
-      response.endHandler(end -> System.out.println("response end"));
-      response.exceptionHandler(event -> {
-        System.out.println("response exception");
-        vertx.cancelTimer(timerId.get());
-        event.printStackTrace();
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+        Router router = Router.router(vertx);
+        vertx.createHttpServer().requestHandler(router).listen(10789);
+        router.get("/test/chunk").handler(routingContext -> {
+            System.out.println("收到请求");
+            HttpServerResponse response = routingContext.response();
+            response.setChunked(true);
+            AtomicInteger i = new AtomicInteger();
+            AtomicReference<Long> timerId = new AtomicReference<>();
+            vertx.setPeriodic(1000, id -> {
+                timerId.set(id);
+                String s = LocalDateTime.now().toString();
+                System.out.println("输出中:" + s);
+                response.write(s);
+                response.write("\n");
+                if (i.getAndIncrement() == 10) {
+                    vertx.cancelTimer(timerId.get());
+                    System.out.println("输出完毕");
+                    response.end();//服务端发送结束符
+                }
+            });
+            response.closeHandler(close -> System.out.println("response close"));
+            response.endHandler(end -> System.out.println("response end"));
+            response.exceptionHandler(event -> {
+                System.out.println("response exception");
+                vertx.cancelTimer(timerId.get());
+                event.printStackTrace();
 
-      });
-    });
+            });
+        });
 
-  }
+    }
 }
