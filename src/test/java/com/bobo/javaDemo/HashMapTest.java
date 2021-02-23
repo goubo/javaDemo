2d3f6f5f36a72233679caa54cad165f275a18c8b
bobo <goujinbo@outlook.com>
2021-02-23 14:39:41 +0800

    添加src

---
+++ src/test/java/com/bobo/javaDemo/HashMapTest.java
@@ -0,0 +1,17 @@
+package com.bobo.javaDemo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * test
+ *
+ * @author BO
+ * @date 2021-02-23 14:33
+ * @since 2021/2/23
+ **/
+public class HashMapTest {
+    public static void main(String[] args) {
+        Map<String, String> string = new HashMap<>();
+    }
+}
